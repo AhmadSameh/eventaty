a84b0b1b6d2643f34f4eff070ffbb92daa266087
AhmadSameh <ahmadsameh660@gmail.com>
2025-10-08 21:03:37 +0300

    add edit and add activity handlers

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -14,6 +15,7 @@ builder.Services.AddCors();
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>()
 );
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
 var app = builder.Build();
 
