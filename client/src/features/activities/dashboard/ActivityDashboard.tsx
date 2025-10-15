fc5d6e0a09f722317d9ad6e2da758a5d9ae48fb5
AhmadSameh <ahmadsameh660@gmail.com>
2025-10-15 15:54:27 +0300

    add activity form

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetail from "../details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
     activities: Activity[];
@@ -30,6 +31,7 @@ export default function ActivityDashboard({
                         cancelSelectActivity={cancelSelectActivity}
                     />
                 }
+                <ActivityForm/>
             </Grid>
         </Grid>
     )
--- client/src/features/activities/details/ActivityDetail.tsx
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -12,11 +12,13 @@ export default function ActivityDetail({ activity, cancelSelectActivity }: Props
                 component='img'
                 src={`/images/categoryImages/${activity.category}.jpg`}
             />
+
             <CardContent>
                 <Typography variant='h5'>{activity.title}</Typography>
                 <Typography variant='subtitle1' fontWeight='light'>{activity.date}</Typography>
                 <Typography variant='body1'>{activity.description}</Typography>
             </CardContent>
+
             <CardActions>
                 <Button color='primary'>Edit</Button>
                 <Button onClick={cancelSelectActivity} color='inherit'>Cancel</Button>
