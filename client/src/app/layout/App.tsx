854e0bc0db85fece21519f90b8338e4120bb8b7c
AhmadSameh <ahmadsameh660@gmail.com>
2025-10-16 13:13:20 +0300

    add react query package

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,23 +1,24 @@
-import { Box, Container, CssBaseline } from "@mui/material";
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-    const [activities, setActivities] = useState<Activity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
     const [editMode, setEditMode] = useState(false);
 
-    useEffect(() => {
-        axios.get<Activity[]>("/api/activities")
-            .then((response => setActivities(response.data)));
-
-        return () => { }
-    }, []);
+    const { data: activities, isPending } = useQuery({
+        queryKey: ['activities'],
+        queryFn: async () => {
+            const respnose = await axios.get<Activity[]>("http://localhost:5001/api/activities");
+            return respnose.data;
+        }
+    });
 
     const handleSelectActivity = (id: string) => {
-        setSelectedActivity(activities.find(activity => activity.id === id));
+        setSelectedActivity(activities!.find(activity => activity.id === id));
     }
 
     const handleCancelSelectActivity = () => {
@@ -35,35 +36,41 @@ function App() {
     }
 
     const handleSubmitForm = (activity: Activity) => {
-        if(activity.id) {
-            setActivities(activities.map(_activity => _activity.id === activity.id ? activity : _activity));
-        } else {
-            const newActivity = {...activity, id: activities.length.toString()};
-            setActivities([...activities, newActivity]);
-        }
+        // if (activity.id) {
+        //     setActivities(activities.map(_activity => _activity.id === activity.id ? activity : _activity));
+        // } else {
+        //     const newActivity = { ...activity, id: activities.length.toString() };
+        //     setActivities([...activities, newActivity]);
+        // }
+        console.log(activity);
         setEditMode(false);
     }
 
     const handleDelete = (id: string) => {
-        setActivities(activities.filter(activity => activity.id !== id));
+        // setActivities(activities.filter(activity => activity.id !== id));
+        console.log(id);
     }
 
     return (
-        <Box sx={{ bgcolor: '#eeeeee' }}>
+        <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
             <CssBaseline />
             <NavBar openForm={handleOpenForm} />
             <Container maxWidth='xl' sx={{ mt: 3 }}>
-                <ActivityDashboard
-                    activities={activities}
-                    selectActivity={handleSelectActivity}
-                    cancelSelectActivity={handleCancelSelectActivity}
-                    selectedActivity={selectedActivity}
-                    editMode={editMode}
-                    openForm={handleOpenForm}
-                    closeForm={handleFormClose}
-                    submitForm={handleSubmitForm}
-                    deleteActivity={handleDelete}
-                />
+                {!activities || isPending ? (
+                    <Typography>Loading...</Typography>
+                ) : (
+                    <ActivityDashboard
+                        activities={activities}
+                        selectActivity={handleSelectActivity}
+                        cancelSelectActivity={handleCancelSelectActivity}
+                        selectedActivity={selectedActivity}
+                        editMode={editMode}
+                        openForm={handleOpenForm}
+                        closeForm={handleFormClose}
+                        submitForm={handleSubmitForm}
+                        deleteActivity={handleDelete}
+                    />
+                )}
             </Container>
         </Box>
     );
