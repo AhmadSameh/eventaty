7162ea226f7c8341e2d2d8ea6d4664650c065caa
AhmadSameh <ahmadsameh660@gmail.com>
2025-10-21 12:17:48 +0300

    add react query fetching

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,15 +1,18 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
+import { useLocation } from "react-router";
 
 export const useActivities = (id?: string) => {
     const queryClient = useQueryClient();
+    const location = useLocation();
 
     const { data: activities, isPending } = useQuery({
         queryKey: ['activities'],
         queryFn: async () => {
             const respnose = await agent.get<Activity[]>('activities');
             return respnose.data;
-        }
+        },
+        enabled: !id && location.pathname === '/activities'
     });
 
     const { data: activity, isLoading: isLoadingActivity } = useQuery({
